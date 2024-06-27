4671ebf5be4c5e3fc3bf2093fbfb8cf140823300
Cinemahubbots <126318209+Cinemahubbots@users.noreply.github.com>
2024-06-28 01:16:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -35,7 +35,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # FSUB
-auth_channel = environ.get('AUTH_CHANNEL','--1002151989500')
+auth_channel = environ.get('AUTH_CHANNEL','--1002243004705')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 # Set to False inside the bracket if you don't want to use Request Channel else set it to Channel ID
 REQ_CHANNEL = environ.get("REQ_CHANNEL",'-1002243004705')
